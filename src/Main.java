cb55e658339fdb7438d1646ed4c67e32d7ea07c4
shirruso <shirru@post.bgu.ac.il>
2021-12-10 17:05:21 +0200

    change in Main

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         String line = "";
         String splitBy = ",";
         try {
-            System.out.println("3");
+            System.out.println("4");
             //parsing a CSV file into BufferedReader class constructor
             BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\shirr\\Documents\\Courses\\MiniProject\\K_anonymization\\src\\healthcare-dataset-stroke-data.csv"));
             int counter = 100;
